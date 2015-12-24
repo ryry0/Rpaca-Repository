58c3cefa70389c23759b208344d199f82cdf4d07
Ryan Reyes <ryan.david.reyes@gmail.com>
2015-12-23 23:31:57 -0500

    Updated massfiles.sh.
    massfiles can now handle filenames with multiple hyphens before the version number.

--- massfiles.sh
+++ massfiles.sh
@@ -4,9 +4,11 @@ OBJECTS=$(cat $1)
 
 for i in $OBJECTS; do
 
-cp -v rpaca-repo/xtrans.rpac $(echo $i | cut -f1 -d- | tr '[:upper:]' '[:lower:]').rpac
-sed -i "s/xtrans/$(echo $i | cut -f1 -d-)/" $(echo $i | cut -f1 -d- | tr '[:upper:]' '[:lower:]').rpac
-sed -i "s/1.3.5/$(echo $i | cut -f2 -d-)/" $(echo $i | cut -f1 -d- | tr '[:upper:]' '[:lower:]').rpac
+FILENAME=$(echo $i | sed "s/-[0-9].[0-9].[0-9]//" | tr '[:upper:]' '[:lower:]')
+
+cp -v bdftopcf.rpac $FILENAME.rpac
+sed -i "s/bdftopcf/$(echo $i | sed "s/-[0-9].[0-9].[0-9]//" )/" $FILENAME.rpac
+sed -i "s/1.0.5/$(echo $i | grep -o '[0-9]\.[0-9]\.[0-9]')/" $FILENAME.rpac
 
 done
 
